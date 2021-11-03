78c4ed337e097ab089bc7ea13ccd01991110e8f0
joaor <joaor@LAPTOP-KTTIAGV0>
2021-11-03 17:07:45 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Contexto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CadastroPessoas.Models
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Pessoa> Pessoas { get; set; }
+
+        public Contexto(DbContextOptions<Contexto> opcoes) : base(opcoes)
+        {
+
+        }
+        
+
+        
+
+
+
+    }
+}
